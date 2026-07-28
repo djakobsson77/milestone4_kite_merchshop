99ae9b731fbbddafa10d5e004082396fc8b1fba0
djakobsson77 <djakobsson77@gmail.com>
2026-07-28 23:59:37 +0200

    Creating store.html and product_detail.html

--- store/models.py
+++ store/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    CATEGORY_CHOICES = [
+        ("tshirt", "T-shirts"),
+        ("longsleeve", "Longsleeve & Hoodies"),
+        ("poster", "Posters"),
+        ("vinyl", "Vinyl & Special Editions"),
+    ]
+
+    name = models.CharField(max_length=200)
+    category = models.CharField(max_length=20, choices=CATEGORY_CHOICES)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    description = models.TextField(blank=True)
+    image = models.ImageField(upload_to="products/", blank=True)
+
+    def __str__(self):
+        return self.name
--- store/views.py
+++ store/views.py
@@ -1,6 +1,12 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Product
 from django.shortcuts import render
 
 # Create your views here.
+def store(request):
+    products = Product.objects.all()
+    return render(request, "store/store.html", {"products": products})
 
-def product_list(request):
-    return render(request, 'store/product_list.html')
+def product_detail(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    return render(request, "store/product_detail.html", {"product": product})
