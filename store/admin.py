9f031095c8ee150950bc9648da8a360a7fd6e057
djakobsson77 <djakobsson77@gmail.com>
2026-08-09 01:47:02 +0200

    Final commit, finalizing README

--- store/admin.py
+++ store/admin.py
@@ -2,11 +2,14 @@ from django.contrib import admin
 from .models import Product, CartItem
 
 # Register your models here.
+
+
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ("name", "category", "price", "stock")
     list_filter = ("category",)
     search_fields = ("name",)
+
 
 @admin.register(CartItem)
 class CartItemAdmin(admin.ModelAdmin):
