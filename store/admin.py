5d91b46e1fbf70976b7bc5bf564e900c7e945a3a
djakobsson77 <djakobsson77@gmail.com>
2026-08-09 00:05:14 +0200

    registering the models in admin.py

--- store/admin.py
+++ store/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
-from .models import Product
+from .models import Product, CartItem
 
 # Register your models here.
-admin.site.register(Product)
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ("name", "category", "price", "stock")
+    list_filter = ("category",)
+    search_fields = ("name",)
+
+@admin.register(CartItem)
+class CartItemAdmin(admin.ModelAdmin):
+    list_display = ("user", "product", "quantity")
+    list_filter = ("user", "product")
